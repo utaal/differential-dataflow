2dec6de9bc31276307c372a43e05c4e537c1a9e0
Andrea Lattuada <andrea.lattuada@inf.ethz.ch>
2019-12-21 22:11:41 +0100

    update graspan for multi-core experiments

--- experiments/src/bin/graspan-interactive.rs
+++ experiments/src/bin/graspan-interactive.rs
@@ -2,6 +2,9 @@ extern crate timely;
 extern crate graph_map;
 extern crate differential_dataflow;
 
+#[cfg(feature = "jemalloc")]
+extern crate jemallocator;
+
 use std::io::{BufRead, BufReader};
 use std::fs::File;
 
@@ -12,9 +15,19 @@ use differential_dataflow::input::Input;
 use differential_dataflow::operators::*;
 use differential_dataflow::operators::arrange::ArrangeByKey;
 
+#[cfg(feature = "jemalloc")]
+#[global_allocator] static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
 fn main() {
 
-    timely::execute_from_args(std::env::args(), move |worker| {
+    let mut args = std::env::args();
+    let _ = args.next().unwrap();
+    let filename: String = args.next().expect("expected filename");
+    let zerocopy_workers: usize = args.next().expect("expected zerocopy_workers").parse().expect("invalid zerocopy_workers");
+
+    let allocators =
+        ::timely::communication::allocator::zero_copy::allocator_process::ProcessBuilder::new_vector(zerocopy_workers);
+    timely::execute::execute_from(allocators, Box::new(()), move |worker| {
 
         let timer = ::std::time::Instant::now();
 
@@ -53,9 +66,7 @@ fn main() {
 
         if index == 0 { println!("{:?}:\tDataflow assembled", timer.elapsed()); }
 
-        // snag a filename to use for the input graph.
-        let filename = std::env::args().nth(1).unwrap();
-        let file = BufReader::new(File::open(filename).unwrap());
+        let file = BufReader::new(File::open(filename.clone()).unwrap());
         for readline in file.lines() {
             let line = readline.ok().expect("read error");
             if !line.starts_with('#') && line.len() > 0 {
--- experiments/src/bin/graspan2.rs
+++ experiments/src/bin/graspan2.rs
@@ -2,6 +2,9 @@ extern crate timely;
 extern crate graph_map;
 extern crate differential_dataflow;
 
+#[cfg(feature = "jemalloc")]
+extern crate jemallocator;
+
 use std::io::{BufRead, BufReader};
 use std::fs::File;
 
@@ -15,26 +18,39 @@ use differential_dataflow::input::Input;
 use differential_dataflow::operators::*;
 use differential_dataflow::operators::arrange::ArrangeByKey;
 
+#[cfg(feature = "jemalloc")]
+#[global_allocator] static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
 type Iter = u64;
 type Difference = i64;
 
 fn main() {
-    if std::env::args().any(|x| x == "optimized") {
-        optimized();
-    }
-    else {
-        unoptimized();
+    let mut args = std::env::args();
+    let _ = args.next().unwrap();
+    let use_optimized: bool = args.next().expect("expected optimize").parse().expect("invalid optimize (bool)");
+    let filename: String = args.next().expect("expected filename");
+    let zerocopy_workers: usize = args.next().expect("expected zerocopy_workers").parse().expect("invalid zerocopy_workers");
+
+    if use_optimized {
+        optimized(filename, zerocopy_workers);
+    } else {
+        unoptimized(filename, zerocopy_workers);
     }
 }
 
-fn unoptimized() {
+fn unoptimized(filename: String, zerocopy_workers: usize) {
 
-    timely::execute_from_args(std::env::args(), move |worker| {
-
-        let timer = ::std::time::Instant::now();
+    let allocators =
+        ::timely::communication::allocator::zero_copy::allocator_process::ProcessBuilder::new_vector(zerocopy_workers);
+    timely::execute::execute_from(allocators, Box::new(()), move |worker| {
 
         let peers = worker.peers();
         let index = worker.index();
+
+        let core_ids = core_affinity::get_core_ids().unwrap();
+        core_affinity::set_for_current(core_ids[index % core_ids.len()]);
+
+        let timer = ::std::time::Instant::now();
 
         let (mut a, mut d) = worker.dataflow::<(),_,_>(|scope| {
 
@@ -129,9 +145,7 @@ fn unoptimized() {
 
         if index == 0 { println!("{:?}:\tDataflow assembled", timer.elapsed()); }
 
-        // snag a filename to use for the input graph.
-        let filename = std::env::args().nth(1).unwrap();
-        let file = BufReader::new(File::open(filename).unwrap());
+        let file = BufReader::new(File::open(filename.clone()).unwrap());
         for readline in file.lines() {
             let line = readline.ok().expect("read error");
             if !line.starts_with('#') && line.len() > 0 {
@@ -161,14 +175,19 @@ fn unoptimized() {
     }).unwrap();
 }
 
-fn optimized() {
+fn optimized(filename: String, zerocopy_workers: usize) {
 
-    timely::execute_from_args(std::env::args(), move |worker| {
-
-        let timer = ::std::time::Instant::now();
+    let allocators =
+        ::timely::communication::allocator::zero_copy::allocator_process::ProcessBuilder::new_vector(zerocopy_workers);
+    timely::execute::execute_from(allocators, Box::new(()), move |worker| {
 
         let peers = worker.peers();
         let index = worker.index();
+
+        let core_ids = core_affinity::get_core_ids().unwrap();
+        core_affinity::set_for_current(core_ids[index % core_ids.len()]);
+
+        let timer = ::std::time::Instant::now();
 
         let (mut a, mut d) = worker.dataflow::<(),_,_>(|scope| {
 
@@ -245,9 +264,7 @@ fn optimized() {
 
         if index == 0 { println!("{:?}:\tDataflow assembled", timer.elapsed()); }
 
-        // snag a filename to use for the input graph.
-        let filename = std::env::args().nth(1).unwrap();
-        let file = BufReader::new(File::open(filename).unwrap());
+        let file = BufReader::new(File::open(filename.clone()).unwrap());
         for readline in file.lines() {
             let line = readline.ok().expect("read error");
             if !line.starts_with('#') && line.len() > 0 {
